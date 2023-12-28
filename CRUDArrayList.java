d21b2120e32ca14beb23f3fcce0eab09357e4bca
DanielBs95 <cleysenenjinia@hotmail.com>
2023-12-28 19:00:10 +0100

    Crud con ArrayList

---
+++ CRUDArrayList.java
@@ -0,0 +1,17 @@
+package crudarraylist;
+
+import javax.swing.JTable;
+
+/**
+ *
+ * @author cleys
+ */
+public class CRUDArrayList {
+    
+    public static void main(String[] args) {
+        Vista vista = new Vista();
+        vista.setVisible(true);
+        
+//        System.out.println(new JTable().getColumnCount(););
+    }
+}
